ba27d4fccd44db5869e24f80decc908faeaa4ac6
Ed <edvardkarch@gmail.com>
2020-04-09 23:11:07 +0300

    init : done task 2

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -1,9 +1,29 @@
 package subtask2
-
 class TimeConverter {
-
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
-    }
+        var hours = hour.toInt()
+        var minutes = minute.toInt()
+        val nums = arrayOf<String>("zero", "one", "two",
+            "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
+            "seventeen", "eighteen", "nineteen", "twenty", "twenty one", "twenty two",
+            "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven",
+            "twenty eight", "twenty nine")
+        return when  (minutes) {
+            0 -> nums[hours] + " o' clock"
+            15 -> "quarter past " + nums[hours]
+            30 -> "half past " + nums[hours]
+            45 -> "quarter to " + nums[(hours % 12) + 1]
+            in (1..29) -> nums[minutes] + " minutes past " +
+                    nums[hours]
+            in (30..59) -> nums[60 - minutes] + " minutes to " +
+                    nums[(hours % 12) + 1]
+
+            else -> ""
+        }
+
 }
+}
+
+

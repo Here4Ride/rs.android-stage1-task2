d7d2a6e03492deda7cabeef805fd0d79c8c60faa
Ed <edvardkarch@gmail.com>
2020-04-14 11:28:36 +0300

    init : done task 4,5,6

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,48 @@
 package subtask4
-
 class Pangram {
-
-    // TODO: Complete the following function
-    fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+    fun Pangram(string: String): Boolean {
+        var alphabet = mutableListOf<Char>('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+        string.forEach {
+            when {(alphabet.contains(it.toLowerCase()))
+            -> alphabet.remove(it.toLowerCase()) }
+        }
+        return alphabet.isEmpty()
     }
+    fun getResult(inputString: String): String {
+        val vowel = charArrayOf('a', 'e', 'i', 'o', 'u', 'y')
+        val words = inputString.split(" ").filter { it != "\n" && it != "" }.toMutableList()
+        if(Pangram(inputString)) {
+            for (i in words.indices) {
+                var pos = 0
+                val word = words[i].toCharArray()
+                for (char in word.indices) {
+                    if (vowel.contains(word[char].toLowerCase())) {
+                        word[char] = word[char].toUpperCase()
+                        pos += 1
+                    }
+                }
+                words[i] = pos.toString() + word.joinToString("")
+            }
+            var result = words.sortedBy { it.first() }.joinToString(" ")
+
+            return result
+        } else {
+            for (i in words.indices) {
+                var pos = 0
+                val word = words[i].toCharArray()
+                for (char in word.indices) {
+                    if (!vowel.contains(word[char].toLowerCase()) && word[char].isLetter()) {
+                        word[char] = word[char].toUpperCase()
+                        pos += 1
+                    }
+                }
+                words[i] = pos.toString() + word.joinToString("")
+            }
+            var result = words.sortedBy { it.first() }.joinToString(" ")
+
+            return result
+        }
+    }
+
 }
--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -1,9 +1,26 @@
 package subtask6
 
 class Fibonacci {
-
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+        var F = intArrayOf(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233)
+        var prod = 0
+        var count = 0
+        var answ = 0
+        while (0<1){
+            prod = F[count]* F[count+1]
+            if (prod == n){
+                answ = 1
+                break}
+            else if (prod> n ){
+                break
+            }
+            count++
+            }
+        if(answ==1){
+            return intArrayOf(F[count], F[count + 1], 1)
+        }
+        else{
+            return intArrayOf(F[count], F[count + 1], 0)
+        }
     }
 }
